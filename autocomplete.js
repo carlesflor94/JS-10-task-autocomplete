9844bb2d0adefe3f5c06f56535a4f23987b721c8
carlesflor94 <cf.ferrandis@gmail.com>
2026-01-21 09:45:04 +0100

    script updated

--- autocomplete.js
+++ autocomplete.js
@@ -1,2 +1,74 @@
-const input = document.querySelector('search__text');
+const input = document.querySelector('.search__text');
 const autocomplete = document.querySelector('.search__results');
+const repository = document.querySelector('.repositories__list');
+
+function addAutocomplete(items) {
+    items.slice(0, 5).forEach(repo => {
+        const item = document.createElement('div');
+        item.textContent = repo.full_name;
+        item.classList.add('autocomplete-item');
+
+        item.addEventListener('click', () => {
+            addRepositories(repo);
+            input.value = '';
+            autocomplete.innerHTML = '';
+            autocomplete.style.display = 'none';
+        });
+
+        autocomplete.appendChild(item);
+    });
+
+    autocomplete.style.display = 'block';
+}
+
+function searchRepositories(query) {
+
+    if (!query.trim()) {
+        autocomplete.innerHTML = '';
+        autocomplete.style.display = 'none';
+        return;
+    }
+
+    else {
+        fetch(`https://api.github.com/search/repositories?q=${query}`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                if (data.items) {
+                    addAutocomplete(data.items);
+                } 
+                else {
+                    addAutocomplete([]);
+                    }
+            })
+            .catch(error => {
+                console.error('Repositories not fetched');
+            })
+        }
+    
+}
+
+function addRepositories(repo) {
+    const item = document.createElement('div');
+    item.classList.add('repositories-item');
+    const githubData = document.createElement('span');
+    githubData.textContent = `${repo.name} | ${repo.owner.login} | ⭐ ${repo.stargazers_count}`;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+
+    deleteButton.addEventListener('click', () => {
+        item.remove();
+    })
+
+    item.appendChild(info);
+    item.appendChild(deleteButton);
+    repository.appendChild(item);
+}
+
+
+input.addEventListener('input', (event) => {
+    const query = event.target.value;
+    searchRepositories(query);
+});
